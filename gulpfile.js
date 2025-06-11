43811949483ff0f1a65fd81c68f0b98c448de0c5
williampeixinho <wd.peixinho@hotmail.com>
2025-06-11 13:38:26 -0300

    criando o hero e importanto a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,32 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+const imagemin = require('gulp-imagemin');
 
-
+// Função para compilar SASS
 function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(plumber()) // impede que o processo quebre em erro
-        .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(plumber())
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/styles'));
 }
-exports.default = styles;
-exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+
+// Função para otimizar imagens
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(plumber())
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
 }
+
+// Função para assistir alterações nos arquivos
+function watchFiles() {
+    gulp.watch('./src/styles/*.scss', styles);
+    gulp.watch('./src/images/**/*', images);
+}
+
+// Exporta as funções
+exports.styles = styles;
+exports.images = images;
+exports.watch = watchFiles;
+exports.default = gulp.parallel(styles, images, watchFiles);
