a4ddf2388c139bf0fa8080627898d1278a0ebe66
williampeixinho <wd.peixinho@hotmail.com>
2025-08-04 15:17:30 -0300

    fix mobile page

--- src/main.js
+++ src/main.js
@@ -1,7 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const tabs = document.querySelectorAll('[data-tab-id]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+        
+        if (posicaoAtual < alturaHero ) {
+            ocutaElementosDoHeader();
+        }
+        else {
+            exibeElementosDoHeader();
+        }
+        
+    })
+    
+    function ocutaElementosDoHeader() {
+        const header = document.querySelector('header');
+        header.classList.add('header--is-hidden');
+    }
+
+    function exibeElementosDoHeader() {
+        const header = document.querySelector('header');
+        header.classList.remove('header--is-hidden');
+    }
+    
+    //Faq
     buttons.forEach(button => {
         button.addEventListener('click', () => {
             const target = button.dataset.tabButton;
@@ -25,5 +52,17 @@ document.addEventListener('DOMContentLoaded', function () {
             // Marca o botão como ativo
             button.classList.add('shows__tabs__button--is-active');
         });
+        
+        for (let i = 0; i < questions.length; i++) {
+            questions[i].addEventListener('click',abreOuFechaResposta);
+        };
+
+        function abreOuFechaResposta(elemento) {
+        const classe = 'faq__questions__item--is-open';
+            const elementoPai = elemento.target.parentNode;
+
+            elementoPai.classList.toggle(classe);
+        }
+
     });
 });
